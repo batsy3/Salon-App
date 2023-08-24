1412f1d6329eed607805f279049aa29eeeda04ac
D!3 4 UZ! <dvdokuku@gmail.com>
2023-08-24 14:07:06 +0200

    bug fix

--- components/supplies/index.tsx
+++ components/supplies/index.tsx
@@ -1,93 +1,107 @@
-import {Button, Input, Text} from '@nextui-org/react';
-import Link from 'next/link';
-import React from 'react';
-import {Breadcrumbs, Crumb, CrumbLink} from '../breadcrumb/breadcrumb.styled';
-import {DotsIcon} from '../icons/accounts/dots-icon';
-import {ExportIcon} from '../icons/accounts/export-icon';
-import {InfoIcon} from '../icons/accounts/info-icon';
-import {TrashIcon} from '../icons/accounts/trash-icon';
-import {HouseIcon} from '../icons/breadcrumb/house-icon';
-import {UsersIcon} from '../icons/breadcrumb/users-icon';
-import {SettingsIcon} from '../icons/sidebar/settings-icon';
-import {Flex} from '../styles/flex';
-import {TableWrapper} from '../table/table';
-import {AddUser} from './add-user';
-import { GetStaticProps, InferGetStaticPropsType } from 'next'
-import { ICategory } from '../../interfaces/supplies.interface';
-import { db } from '../../pages/api/firestore';
+import {  Input, Text } from "@nextui-org/react";
+import Link from "next/link";
+import React from "react";
+import {
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownItem,
+  Button
+} from "@nextui-org/react";
 
-const getStaticProps: GetStaticProps<{ props: ICategory }> = async () => {
-   await db.collection("Category").getAll()
-   return 
-} 
+import { Breadcrumbs, Crumb, CrumbLink } from "../breadcrumb/breadcrumb.styled";
+import { DotsIcon } from "../icons/accounts/dots-icon";
+import { ExportIcon } from "../icons/accounts/export-icon";
+import { InfoIcon } from "../icons/accounts/info-icon";
+import { TrashIcon } from "../icons/accounts/trash-icon";
+import { HouseIcon } from "../icons/breadcrumb/house-icon";
+import { UsersIcon } from "../icons/breadcrumb/users-icon";
+import { SettingsIcon } from "../icons/sidebar/settings-icon";
+import { Flex } from "../styles/flex";
+import { TableWrapper } from "../table/table";
+import { AddUser } from "./add-user";
+import { GetStaticProps, InferGetStaticPropsType } from "next";
+import { ICategory } from "../../interfaces/supplies.interface";
+import { db } from "../../pages/api/firestore";
 
-export const Supplies = () => {
-   return (
-      <Flex
-         css={{
-            'mt': '$5',
-            'px': '$6',
-            '@sm': {
-               mt: '$10',
-               px: '$16',
-            },
-         }}
-         justify={'center'}
-         direction={'column'}
-      >
-         <Breadcrumbs>
-            <Crumb>
-               <HouseIcon />
-               <Link href={'/'}>
-                  <CrumbLink href="/">Home</CrumbLink>
-               </Link>
-               <Text>/</Text>
-            </Crumb>
-
-            <Crumb>
-               <UsersIcon />
-               <CrumbLink href="/supplies">Supplies</CrumbLink>
-               <Text>/</Text>
-            </Crumb>
-            {/* <Crumb>
-               <CrumbLink href="#">List</CrumbLink>
-            </Crumb> */}
-         </Breadcrumbs>
-
-         <Text h3>All Accounts</Text>
-         <Flex
-            // css={{gap: '$8'}}
-            align={'center'}
-            justify={'between'}
-            wrap={'wrap'}
-         >
-            <Flex
-               css={{
-                  'gap': '$6',
-                  'flexWrap': 'wrap',
-                  '@sm': {flexWrap: 'nowrap'},
-               }}
-               align={'center'}
-            >
-               <Input
-                  css={{width: '100%', maxW: '410px'}}
-                  placeholder="Search users"
-               />
-               <SettingsIcon />
-               <TrashIcon />
-               <InfoIcon />
-               <DotsIcon />
-            </Flex>
-            <Flex direction={'row'} css={{gap: '$6'}} wrap={'wrap'}>
-               <AddUser />
-               <Button auto iconRight={<ExportIcon />}>
-                  Export to CSV
-               </Button>
-            </Flex>
-         </Flex>
-
-         <TableWrapper />
-      </Flex>
-   );
+const getStaticProps: GetStaticProps<ICategory> = async () => {
+  const res = await db.collection("Category").get();
+  return { props: { categories: res } };
 };
 
+export const Supplies = ({ categories }) => {
+  return (
+    <Flex
+      css={{
+        mt: "$5",
+        px: "$6",
+        "@sm": {
+          mt: "$10",
+          px: "$16",
+        },
+      }}
+      justify={"center"}
+      direction={"column"}
+    >
+      <Breadcrumbs>
+        <Crumb>
+          <HouseIcon />
+          <Link href={"/"}>
+            <CrumbLink href="/">Home</CrumbLink>
+          </Link>
+          <Text>/</Text>
+        </Crumb>
+
+        <Crumb>
+          <UsersIcon />
+          <CrumbLink href="/supplies">Supplies</CrumbLink>
+          <Text>/</Text>
+        </Crumb>
+        {/* <Crumb>
+               <CrumbLink href="#">List</CrumbLink>
+            </Crumb> */}
+      </Breadcrumbs>
+
+      <Text h3>All Accounts</Text>
+      <Flex
+        //   css={{gap: '$8'}}
+        align={"center"}
+        justify={"between"}
+        wrap={"wrap"}
+      >
+        <Flex
+          css={{
+            gap: "$6",
+            flexWrap: "wrap",
+            "@sm": { flexWrap: "nowrap" },
+            marginBottom: 15,
+          }}
+          align={"center"}
+        >
+          <Input
+            css={{ width: "100%", maxW: "410px" }}
+            placeholder="Search users"
+          />
+          <Dropdown>
+            <DropdownTrigger>
+              <Button variant="bordered">Filter</Button>
+            </DropdownTrigger>
+            <DropdownMenu aria-label="Static Actions">
+              {/* {categories?.map((item, index) => (
+                <DropdownItem key={index}>{item.name}</DropdownItem>;
+              ))} */}
+            </DropdownMenu>
+          </Dropdown>
+        </Flex>
+        <Flex direction={"row"} css={{ gap: "$6" }} wrap={"wrap"}>
+          <AddUser />
+          <Button auto iconRight={<ExportIcon />}>
+            Export to CSV
+          </Button>
+        </Flex>
+      </Flex>
+
+      <TableWrapper />
+    </Flex>
+  );
+};
--- pages/accounts.tsx
+++ pages/accounts.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React from "react";
 import { Supplies } from "../components/supplies";
 
 const supplies = () => {
-   return <Supplies />;
+  return <Supplies />;
 };
 
 export default supplies;
